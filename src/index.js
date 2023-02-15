f70f489cfbe12e03c5dc9f4296af010d912ded5e
Jaithecomder <jaib2910@gmail.com>
2023-02-15 18:28:28 +0530

    wall coll logic

--- src/index.js
+++ src/index.js
@@ -5,17 +5,24 @@ import { createCube, rotateCube } from './cube';
 import { createLight, moveLight } from './lighting';
 import { createPlane } from './plane';
 
+const startScene = new THREE.Scene();
 const scene = new THREE.Scene();
+
 const aspect = window.innerWidth / window.innerHeight;
 var minimWidth = window.innerHeight / 4;
 var minimHeight = window.innerHeight / 4;
+const stCamera = new THREE.OrthographicCamera( - window.innerWidth / 20, window.innerWidth / 20, window.innerHeight / 20, - window.innerHeight / 20, 1, 1000);
 const camera = new THREE.PerspectiveCamera( 75, aspect, 0.1, 1000 );
 const topcamera = new THREE.OrthographicCamera(- minimWidth / 2, minimWidth / 2, minimHeight / 2, - minimHeight / 2, 1, 1000);
 
+startScene.add(stCamera);
 scene.add(camera);
 scene.add(topcamera);
 
-const renderer = new THREE.WebGLRenderer(); 
+const clock = new THREE.Clock();
+var deltaTime;
+
+const renderer = new THREE.WebGLRenderer({antialias: true}); 
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.outputEncoding = THREE.sRGBEncoding;
 document.body.appendChild( renderer.domElement );
@@ -23,7 +30,8 @@ document.body.appendChild( renderer.domElement );
 const sun = createLight(10, 100, 100, 100);
 const grnd = createPlane(1000, 1000);
 grnd.position.y = 0;
-const lwall = createCube(0.5, 2, 10);
+grnd.rotation.x = Math.PI/2;
+const lwall = createCube(0.5, 2, 200);
 const lwallbox = new THREE.Box3().setFromObject(lwall);
 lwall.geometry.userData.obb = new OBB().fromBox3(lwallbox);
 lwall.userData.obb = new OBB();
@@ -60,11 +68,13 @@ const carBox = new THREE.Box3().setFromObject(car);
 
 var center = new THREE.Vector3();
 carBox.getCenter(center);
+const carHalfDiag = Math.sqrt((center.x - carBox.min.x) * (center.x - carBox.min.x) + (center.z - carBox.min.z) * (center.z - carBox.min.z))
 const carHb = createCube(carBox.max.x - carBox.min.x, carBox.max.y - carBox.min.y, carBox.max.z - carBox.min.z);
 
 carHb.position.sub(center);
 carHb.position.y = center.y - carBox.min.y;
 car.add(carHb);
+carHb.visible = false;
 
 car.position.sub(center);
 
@@ -83,11 +93,11 @@ scene.add(lwall);
 scene.add(rwall);
 scene.background = new THREE.Color('#DEFEFF');
 
-const facc = 0.075;
-const bacc = 0.05;
-const fricacc = 0.05;
-const flim = 1.5;
-const blim = -0.5;
+const facc = 2;
+const bacc = 1;
+const fricacc = 1;
+const flim = 100;
+const blim = -50;
 var speed = 0;
 var turnSp = 0;
 var turnEff = 0;
@@ -96,6 +106,7 @@ var aPress = 0;
 var dPress = 0;
 var wPress = 0;
 var sPress = 0;
+var start = 0;
 var tCam = 0;
 
 document.addEventListener("keypress", onDocumentKeyPress, false);
@@ -111,23 +122,28 @@ function onDocumentKeyPress(event) {
 			car.remove(camera);
 		}
     }
+	if (keyCode == 32) {
+		start = 1;
+	}
 };
 
 document.addEventListener("keydown", onDocumentKeyDown, false);
 function onDocumentKeyDown(event) {
     var keyCode = event.which;
-    if (keyCode == 87) {
-        wPress = 1;
-    }
-	if (keyCode == 83) {
-        sPress = 1;
-    }
-	if (keyCode == 65) {
-		aPress = 1;
-    }
-	if (keyCode == 68) {
-		dPress = 1;
-    }
+	if(start == 1) {
+		if (keyCode == 87) {
+			wPress = 1;
+		}
+		if (keyCode == 83) {
+			sPress = 1;
+		}
+		if (keyCode == 65) {
+			aPress = 1;
+		}
+		if (keyCode == 68) {
+			dPress = 1;
+		}
+	}
 };
 
 document.addEventListener("keyup", onDocumentKeyUp, false);
@@ -175,8 +191,8 @@ function set1PCam() {
 
 
 function carGo(car, speed) {
-	car.position.x = car.position.x - speed * Math.sin(car.rotation.y - Math.PI);
-	car.position.z = car.position.z - speed * Math.cos(car.rotation.y - Math.PI);
+	car.position.x = car.position.x - speed * Math.sin(car.rotation.y - Math.PI) * deltaTime;
+	car.position.z = car.position.z - speed * Math.cos(car.rotation.y - Math.PI) * deltaTime;
 }
 
 function carMove(car) {
@@ -211,8 +227,8 @@ function carMove(car) {
 		}
 	}
 
-	turnSp = speed / 50;
-	turnEff = speed / 50;
+	turnSp = speed / 50 * deltaTime;
+	turnEff = speed / 50 * deltaTime;
 
 	if(speed >= 0) {
 		if(aPress == 1) {
@@ -301,33 +317,43 @@ window.addEventListener("resize", winResize);
 function animate() {
 	requestAnimationFrame( animate );
 
-	grnd.rotation.x = Math.PI/2;
+	if(start == 1) {
+		deltaTime = clock.getDelta();
 
-	carMove(car);
+		carMove(car);
 
-	if(tCam == 0) {
-		set3PCam(camera, car);
+		if(tCam == 0) {
+			set3PCam(camera, car);
+		}
+
+		if(checkColl(lwall , carHb)) {
+			car.rotation.y = lwall.rotation.y - car.rotation.y + 2 * Math.PI;
+			yRot = lwall.rotation.y - yRot;
+			const carWallDist = Math.abs(carHalfDiag * Math.sin(lwall.rotation.y - car.rotation.y + Math.PI));
+			car.position.x = car.position.x + carWallDist * Math.cos(lwall.rotation.y);
+			car.position.z = car.position.z - carWallDist * Math.sin(lwall.rotation.y);
+			speed = speed / 3;
+		}
+		else {
+			lwall.visible = true;
+		}
+
+		renderer.setViewport(0, 0, window.innerWidth, window.innerHeight);
+		renderer.render( scene, camera );
+
+		renderer.clearDepth();
+
+		renderer.setScissorTest(true);
+
+		renderer.setScissor(window.innerWidth - minimWidth - 16, window.innerHeight - minimHeight - 16, minimWidth, minimHeight);
+		renderer.setViewport(window.innerWidth - minimWidth - 16, window.innerHeight - minimHeight - 16, minimWidth, minimHeight);
+		renderer.render(scene, topcamera);
+
+		renderer.setScissorTest(false);
 	}
-
-	if(checkColl(lwall , carHb)) {
-		lwall.visible = false;
+	else if (start == 0) {
+		renderer.render(startScene, stCamera);
 	}
-	else {
-		lwall.visible = true;
-	}
-
-	renderer.setViewport(0, 0, window.innerWidth, window.innerHeight);
-	renderer.render( scene, camera );
-
-	renderer.clearDepth();
-
-	renderer.setScissorTest(true);
-
-	renderer.setScissor(window.innerWidth - minimWidth - 16, window.innerHeight - minimHeight - 16, minimWidth, minimHeight);
-	renderer.setViewport(window.innerWidth - minimWidth - 16, window.innerHeight - minimHeight - 16, minimWidth, minimHeight);
-	renderer.render(scene, topcamera);
-
-	renderer.setScissorTest(false);
 };
 
 animate();
